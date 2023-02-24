32d7e564e9a402f56d81a4c9d4249cceb0965cde
Magomed Muradkhanov <magamuradkhanov@gmail.com>
2023-02-24 18:07:12 +0300

    Init commit

---
+++ src/main/java/com/magatella/cryptoexchangebackend/CryptoExchangeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.magatella.cryptoexchangebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoExchangeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoExchangeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/magatella/cryptoexchangebackend/CryptoExchangeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magatella.cryptoexchangebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoExchangeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
